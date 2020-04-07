46662af119e21d3d6668d6ac4e1215b6bd1a43ad
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-07 05:41:26 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,11 +1,13 @@
 # Write your solution here
 
+# solution one
 if Time.now.to_i % 2 == 0
   puts "Even!"
   else 
   puts "Odd!"
 end 
 
+# solution two 
 current_time = Time.now
 current_time = current_time.to_i 
 
